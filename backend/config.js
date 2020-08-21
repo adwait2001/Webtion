e1fc22d15aefffb6fa10e4d3e2c1b00ef551e9c3
ADWAIT <adwaitnsk.2009@rediffmail.com>
2020-08-21 09:12:20 +0530

    finally

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export default{
-    MONGODB_URL:process.env.MONGODB_URI||'mongodb://localhost/webtion',
+    MONGODB_URL:"mongodb+srv://adwait:adwait@cluster0.fprqi.mongodb.net/adwait?retryWrites=true&w=majority" ||'mongodb://localhost/webtion',
     JWT_SECRET:process.env.JWT_SECRET||'upcomingwebtion',
     PORT:process.env.PORT || 5000,
 }
